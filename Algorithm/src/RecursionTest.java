ead8e59bc837c2e0368d3b071edfe3e5670edb74
wisang eom <wseom7@gmail.com>
2014-01-14 16:42:41 +0900

    fiboancci recursion version
    fiboancci recursion version

--- Algorithm/src/RecursionTest.java
+++ Algorithm/src/RecursionTest.java
@@ -6,8 +6,18 @@ import org.junit.Test;
 public class RecursionTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void fibonacciTest() {
+		assertEquals(1, fibonacci(1));
+		assertEquals(1, fibonacci(2));
+		assertEquals(2, fibonacci(3));
+		assertEquals(3, fibonacci(4));
+		assertEquals(5, fibonacci(5));
+		assertEquals(8, fibonacci(6));
 	}
 
+	private int fibonacci(int n) {
+		if(n==1 || n==2)
+			return 1;
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
 }
