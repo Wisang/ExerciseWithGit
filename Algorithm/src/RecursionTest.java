f8555c78679d14c9dc1321c48302aab10af7c7a8
wisang eom <wseom7@gmail.com>
2014-01-16 17:08:54 +0900

    removed compile error

--- Algorithm/src/RecursionTest.java
+++ Algorithm/src/RecursionTest.java
@@ -17,7 +17,7 @@ public class RecursionTest {
 	private int fibonacci(int n) {
 		if(n==1 || n==2)
 			return 1;
-		//return fibonacci(n-1) + fibonacci(n-2);
+		return fibonacci(n-1) + fibonacci(n-2);
 	}
 	
 	@Test
