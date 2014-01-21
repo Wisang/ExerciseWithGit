ca1b1580dc67ee70e7d1db9da57585b4dc48d1bb
wisang eom <wseom7@gmail.com>
2014-01-21 17:22:39 +0900

    partition problem
    test failed

--- Algorithm/src/NumberPartitionTest.java
+++ Algorithm/src/NumberPartitionTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 
@@ -11,6 +14,24 @@ public class NumberPartitionTest {
 	}
 
 	private int numberOfPartition(int number) {
-		return 3;
+		List<Integer> num = new ArrayList<Integer>();
+		
+		for(int i=1; i<=number; i++)
+			num.add(i);
+		
+		return partition(number, num);
+	}
+
+	private int partition(int number, List<Integer> num) {
+		if(1 == number)
+			return 1;
+		
+		int count = 0;
+		int numberOfWays = number / num.get(num.size()-1);
+		
+		for(int i=0; i<numberOfWays; i++)
+			count += partition(number - i*num.get(num.size()-1), num.subList(0, num.size()-1));
+		
+		return count;
 	}
 }
