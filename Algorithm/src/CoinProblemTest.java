e0f127a07fd39ea239eed8086d00191d94464998
wisang eom <wseom7@gmail.com>
2014-01-21 16:13:58 +0900

    coin problem
    recursion version

--- Algorithm/src/CoinProblemTest.java
+++ Algorithm/src/CoinProblemTest.java
@@ -9,6 +9,30 @@ public class CoinProblemTest {
 		int[] coins = {1000, 100, 10};
 		assertEquals(12, numberOfPaymentLoop(1000, coins));
 	}
+	
+	@Test
+	public void numberOfPaymentRecursionTest() throws Exception {
+		int[] coins = {1,2,5,10,20,50};
+		assertEquals(4562, numberOfPaymentRecursion(100, coins, 6));
+	}
+
+	private int numberOfPaymentRecursion(int money, int[] coins, int n) {
+		
+		if(1 == n) {
+			if(0 == money % coins[0])
+				return 1;
+			return 0;
+		}
+		
+		int numberOfWays = money / coins[n-1];
+		
+		int count = 0;
+		
+		for(int i=0; i<= numberOfWays; i++)
+			count  += numberOfPaymentRecursion(money - i * coins[n-1], coins, n-1);
+			
+		return count;
+	}
 
 	private int numberOfPaymentLoop(int money, int[] coins) {
 		int count = 0;
