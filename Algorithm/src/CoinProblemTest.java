d2e5468f21d20a938f31f8d7c9527a92be9d9eb8
wisang eom <wseom7@gmail.com>
2014-01-21 14:52:52 +0900

    fibonacci loop verstion

--- Algorithm/src/CoinProblemTest.java
+++ Algorithm/src/CoinProblemTest.java
@@ -2,15 +2,32 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class CoinProblemTest {
 
 	@Test
 	public void fibonacciLoopTest() {
 		assertEquals(1, fibonacciLoop(1));
+		assertEquals(1, fibonacciLoop(2));
+		assertEquals(2, fibonacciLoop(3));
+		assertEquals(3, fibonacciLoop(4));
+		assertEquals(5, fibonacciLoop(5));
 	}
 
 	private long fibonacciLoop(int n) {
-		return 0;
+		if (n == 1 || n == 2)
+			return 1;
+
+		long first = 1;
+		long second = 1;
+		long result = 0;
+
+		for(int i=3; i<=n; i++) {
+			result = first + second;
+
+			first = second;
+			second = result;
+		}
+
+		return result;
 	}
 }
