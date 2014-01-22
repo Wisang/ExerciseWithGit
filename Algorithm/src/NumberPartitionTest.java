aaddd1aed25118eb6b48921c6ce53f548f52b190
wisang eom <wseom7@gmail.com>
2014-01-22 13:46:01 +0900

    added comments for the formula

--- Algorithm/src/NumberPartitionTest.java
+++ Algorithm/src/NumberPartitionTest.java
@@ -1,27 +1,54 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-
 public class NumberPartitionTest {
-	
+
 	@Test
 	public void partitionTest() throws Exception {
 		assertEquals(3, numberOfPartition(3));
 		assertEquals(7, numberOfPartition(5));
 	}
-	
+
 	@Test
 	public void generalPartitionTest() throws Exception {
-		assertEquals(3, generalPartition(5,2));
-		assertEquals(5, generalPartition(5,3));
+		assertEquals(3, generalPartition(5, 2));
+		assertEquals(5, generalPartition(5, 3));
+	}
+
+	@Test
+	public void mathPartitionTest() throws Exception {
+		assertEquals(3, recurrenceFormulaPartition(5, 2));
+		assertEquals(5, recurrenceFormulaPartition(5, 3));
+	}
+
+	private int recurrenceFormulaPartition(int n, int m) {
+		// partition(5,2) = partition(4,1) + partition(3,2)
+		// = (3,1) + (2,1) + (1,2)
+		// = (2,1) + (1,1) + (1,1)
+		// = (1,1) + (0,1) + (0,1)
+		// = (0,1) + 1 + 1
+		// = 1 + 1 + 1 =3
+		
+		if (0 == n)
+			return 1;
+		
+		if(n < m)
+			m = n;
+		
+		int count = 0;
+
+		for (int i = 1; i <= m; i++)
+			count += recurrenceFormulaPartition(n - i, i);
+
+		return count;
 	}
 
 	private int generalPartition(int number, int set) {
 		int[] numbers = new int[set];
-		
-		for(int i=1; i<=set; i++)
-			numbers[i-1] = i;
-		
+
+		for (int i = 1; i <= set; i++)
+			numbers[i - 1] = i;
+
 		return partition(number, numbers, set);
 	}
 
@@ -30,16 +57,16 @@ public class NumberPartitionTest {
 	}
 
 	private int partition(int number, int[] numbers, int n) {
-		if(1==n)
+		if (1 == n)
 			return 1;
-		
-		int numberOfWays = number / numbers[n-1];
-		
+
+		int numberOfWays = number / numbers[n - 1];
+
 		int count = 0;
-		
-		for(int i=0; i<=numberOfWays; i++)
-			count += partition(number - i*numbers[n-1], numbers, n-1);
-		
+
+		for (int i = 0; i <= numberOfWays; i++)
+			count += partition(number - i * numbers[n - 1], numbers, n - 1);
+
 		return count;
 	}
 }
