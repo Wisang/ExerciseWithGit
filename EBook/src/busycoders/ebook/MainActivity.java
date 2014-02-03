2a94a6f0a35844e5ecd2a3a4583b87d64e3d134f
wisang eom <wseom7@gmail.com>
2014-02-03 13:05:50 +0900

    list adapter works

--- EBook/src/busycoders/ebook/MainActivity.java
+++ EBook/src/busycoders/ebook/MainActivity.java
@@ -1,22 +1,33 @@
 package busycoders.ebook;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.app.ListActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
+	private TextView selection;
+	private static final String[] items = { "lorem", "ipsum", "dolor", "sit",
+			"amet", "consectetuer", "adipiscing", "elit", "morbi", "vel",
+			"ligula", "vitae", "arcu", "aliquet", "mollis", "etiam", "vel",
+			"erat", "placerat", "ante", "porttitor", "sodales", "pellentesque",
+			"augue", "purus" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, items));
+		selection = (TextView) findViewById(R.id.selection);
+
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onListItemClick(ListView parent, View v, int position, long id) {
+		selection.setText(items[position]);
 	}
 
 }
