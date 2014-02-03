cee326446b8b33c86a3190ba91ab9dc74349a6d7
wisang eom <wseom7@gmail.com>
2014-02-03 14:25:07 +0900

    spinner version

--- EBook/src/busycoders/ebook/MainActivity.java
+++ EBook/src/busycoders/ebook/MainActivity.java
@@ -1,37 +1,47 @@
 package busycoders.ebook;
 
 import android.os.Bundle;
-import android.app.ListActivity;
+import android.app.Activity;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.AdapterView;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity implements
+		AdapterView.OnItemSelectedListener {
 	private TextView selection;
 	private static final String[] items = { "lorem", "ipsum", "dolor", "sit",
 			"amet", "consectetuer", "adipiscing", "elit", "morbi", "vel",
 			"ligula", "vitae", "arcu", "aliquet", "mollis", "etiam", "vel",
 			"erat", "placerat", "ante", "porttitor", "sodales", "pellentesque",
 			"augue", "purus" };
-	
-	private StringBuffer sb;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		setListAdapter(new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_multiple_choice, items));
 		selection = (TextView) findViewById(R.id.selection);
-		sb = new StringBuffer();
+
+		Spinner spin = (Spinner) findViewById(R.id.spinner);
+		spin.setOnItemSelectedListener(this);
+
+		ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, items);
+		aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin.setAdapter(aa);
 
 	}
 
 	@Override
-	public void onListItemClick(ListView parent, View v, int position, long id) {
-		sb.append(items[position] + " ");
-		selection.setText(sb);
+	public void onItemSelected(AdapterView<?> parent, View v, int position,
+			long id) {
+		selection.setText(items[position]);
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		selection.setText("");
 	}
 
 }
