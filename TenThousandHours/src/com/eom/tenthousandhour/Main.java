d79749ce0e259fe48b58c7affe7a532c78632c1f
wisang eom <wseom7@gmail.com>
2014-02-21 14:23:16 +0900

    button added

--- TenThousandHours/src/com/eom/tenthousandhour/Main.java
+++ TenThousandHours/src/com/eom/tenthousandhour/Main.java
@@ -3,20 +3,33 @@ package com.eom.tenthousandhour;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Main extends Activity {
-
+public class Main extends Activity implements OnClickListener{
+	Button btn;
+	TextView tv;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		btn = (Button)findViewById(R.id.getDistanceBtn);
+		btn.setOnClickListener(this);
+		
+		tv = (TextView)findViewById(R.id.remainingDistanceText);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
+	@Override
+	public void onClick(View v) {
+		tv.setText("button pressed");
+	}
 }
